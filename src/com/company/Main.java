bb9bb5020ec905869e1ef36d68ca2fe9a2d96ea7
Williams <WILLIAM24@science.regn.net>
2019-10-08 16:09:30 +0100

    add files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         String helloVar = "Hello World";
-        System.out.println(helloVar);
+        System.out.println(helloVar); //
     }
 }
