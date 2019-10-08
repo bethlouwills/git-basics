ffc9b2480a1b3399bccc54bf13c8b8803c6e4f77
Williams <WILLIAM24@science.regn.net>
2019-10-08 15:48:41 +0100

    add src

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String helloVar = "Hello World";
+        System.out.println(helloVar);
+    }
+}
